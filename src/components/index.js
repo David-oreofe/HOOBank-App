497b7c49dcaeb1b9cd437fdefc4923c6018b1d5c
unknown <oyedokunoreofe@gmail.com>
2023-03-16 22:04:43 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar"
+import Hero from './Hero'
+import Business from './Business'
+import Billing from './Billing'
+import CardDeal from  './CardDeal'
+import Testimonial from './Testimonial'
+import Clients from './Client'
+import Stats from './Stats'
+import CTA from './CTA'
+import Footer from './Footer'
+
+export {
+    Navbar,
+    Hero,
+    Stats,       
+    Business,
+    Billing,               
+    CardDeal,
+    Testimonial,
+    Clients,
+    CTA,
+    Footer
+ }
